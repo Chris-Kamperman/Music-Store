7b22047e17678bcd0cc7e2254808607c7e949cef
Chris <chris.kamperman1@gmail.com>
2025-02-14 23:58:41 +0100

    Added Read And Create For Artists;

---
+++ music-store/app/Http/Controllers/ArtistController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Artist;
+
+class ArtistController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return Artist::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        return Artist::create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        return Artist::FindOrFail($id);
+    }
+}
--- music-store/routes/api.php
+++ music-store/routes/api.php
@@ -3,9 +3,14 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AlbumController;
+use App\Http\Controllers\ArtistController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
 Route::resource('albums', AlbumController::class);
+
+Route::get('/artists', ArtistController::class . '@index');
+Route::get('/artists/{id}', ArtistController::class . '@show');
+Route::post('/artists', ArtistController::class . '@store');
