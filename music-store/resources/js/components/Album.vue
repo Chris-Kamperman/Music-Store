bfc3497a1017a8b4252a8828c49f055f07a7c19a
Chris <chris.kamperman1@gmail.com>
2025-02-16 21:06:21 +0100

    Fixed Static Album Image;

--- music-store/resources/js/components/Album.vue
+++ music-store/resources/js/components/Album.vue
@@ -1,5 +1,5 @@
 <script setup>
-    const props = defineProps(['album'])
+    const props = defineProps(['album']);
 </script>
 
 <template>
@@ -7,8 +7,8 @@
         <div class="flex flex-col md:flex-row w-full">
             <div class="relative p-2.5 md:w-1/5 shrink-0 overflow-hidden">
                 <img
-                src="https://images.unsplash.com/photo-1522202176988-66273c2fd55f?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=1471&amp;q=80"
-                alt="card-image"
+                :src="album.artwork"
+
                 class="h-full w-full rounded-md md:rounded-lg object-cover"
                 />
             </div>
